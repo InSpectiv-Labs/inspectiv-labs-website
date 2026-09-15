29fbc52a27d9e550832f2ca8b73921f27104840e
Arghajit-Saha <arghajitsaha8@gmail.com>
2026-09-15 12:52:47 +0530

    fix: fixed build issues

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,12 +6,14 @@ import sitemap from '@astrojs/sitemap';
 import mdx from '@astrojs/mdx';
 import { remarkReadingTime } from './src/utils/remark-reading-time.mjs';
 import { unified } from '@astrojs/markdown-remark';
-import vercel from '@astrojs/vercel';
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: vercel(),
+  adapter: node({
+    mode: 'standalone'
+  }),
   site: 'https://inspectivlabs.com',
   integrations: [react(), sitemap({ filter: (page) => !page.includes('/admin') }), mdx()],
   markdown: {
