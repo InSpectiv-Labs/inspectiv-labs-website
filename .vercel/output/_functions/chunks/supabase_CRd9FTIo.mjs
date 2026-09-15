87790804c22ab7951e51c34e60e3a408056121ed
Arghajit-Saha <arghajitsaha8@gmail.com>
2026-09-15 13:13:45 +0530

    fix: fixed build issues

--- .vercel/output/_functions/chunks/supabase_CRd9FTIo.mjs
+++
@@ -1,4 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-var supabase = createClient("https://aifqfdxdokhdvipvvoiu.supabase.co", "sb_publishable_Jcvqng7Mxzk7v4Ck89UJ3w_WcgBXYYC");
-//#endregion
-export { supabase as t };
